a6f0ce627b81ae4d08137f09493d171e480e8283
thowfick official <mdthowfickspeed@gmail.com>
2024-01-30 23:33:27 +0530

    email

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { signInWithGoogle } from '../../firebase';
 import { signIn, setUser } from '../../redux/actions/authActions';
 import { useNavigate } from 'react-router-dom';
+import emailjs from 'emailjs-com';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -27,6 +28,9 @@ const Home = () => {
         // Log success message
         console.log('Login successful:', user.displayName);
 
+        // Get user details (IP, location, device) and send an email
+        getUserDetailsAndSendEmail(user);
+
         // Redirect to dashboard after successful login
         navigate('/dashboard');
       } else {
@@ -34,6 +38,40 @@ const Home = () => {
       }
     } catch (error) {
       console.error('Error signing in with Google:', error.message);
+    }
+  };
+
+  const getUserDetailsAndSendEmail = async (user) => {
+    // Fetch user details from IPinfo API
+    try {
+      const response = await fetch('https://ipinfo.io/json?token=ab9e499a944ce8');
+      const data = await response.json();
+
+      // Extract user details
+      const { ip, city, region, country, org } = data;
+
+      // Construct email parameters
+      const emailParams = {
+        to_email: user.email,
+        user_name: user.displayName,
+        user_ip: ip,
+        user_location: `${city}, ${region}, ${country}`,
+        user_device: `${org}`,
+        user_email: user.email,
+        user_display_name: user.displayName,
+        user_photo_url: user.photoURL,
+      };
+
+      // Send an email using emailjs
+      emailjs.send('service_6ploqke', 'template_kpykiwg', emailParams, 'WBwcTzKFut9UhpRjq')
+        .then((response) => {
+          console.log('Email sent successfully:', response);
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error);
+        });
+    } catch (error) {
+      console.error('Error fetching user details:', error);
     }
   };
 
